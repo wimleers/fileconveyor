8361dbe034a9364ffd47a2a62a1fa06890e916f5
Wim Leers <work@wimleers.com>
2009-04-10 01:03:44 +0000

    - The Filter module, allows you to load a set of conditions (paths, extensions, ignoredDirs, pattern and size) to match files against. This will be used to decide what to do with each file. 100% testing coverage! - First commit for the daemon! - First day of working with Python! - First programming language in which lambda functions are actually useful! - First time the entire codebase is covered by unit test from the beginning, even the ones involving file I/O!

---
+++ code/daemon/filter.py
@@ -0,0 +1,180 @@
+"""filter.py Filter class for daemon"""
+
+
+__author__ = "Wim Leers (work@wimleers.com)"
+__version__ = "$Rev$"
+__date__ = "$Date$"
+__license__ = "GPL"
+
+
+from sets import Set, ImmutableSet
+import re
+import types
+import os
+import os.path
+import stat
+
+
+# Define exceptions.
+class FilterError(Exception): pass
+class InvalidConditionError(FilterError): pass
+class MissingConditionError(FilterError): pass
+class InvalidPathsConditionError(InvalidConditionError): pass
+class InvalidExtensionsConditionError(InvalidConditionError): pass
+class InvalidIgnoredDirsConditionError(InvalidConditionError): pass
+class InvalidPatternConditionError(InvalidConditionError): pass
+class InvalidSizeConditionError(InvalidConditionError): pass
+class MatchError(FilterError): pass
+
+
+class Filter(object):
+    """filter filepaths based on path, file extensions, ignored directories, file pattern and file size"""
+
+    valid_conditions = ImmutableSet(["paths", "extensions", "ignoredDirs", "pattern", "size"])
+    required_conditions = ImmutableSet(["paths", "extensions"])
+    required_sizeconditions = ImmutableSet(["conditionType", "treshold"])
+    # Prevent forbidden characters in filepaths!
+    # - Mac OS X: :
+    # - Linux: /
+    # - Windows: * " / \ [ ] : ; | = , < >
+    # It's clear that if your filepaths are valid on Windows, they're valid
+    # anywhere. So we go with that.
+    forbidden_characters = {
+        "paths"       : '\*"\[\]:;\|=,<>',      # / and \ are allowed
+        "extensions"  : '\*"/\\\[\]:;\|=,<>\.', # / and \ and . are disallowed
+        "ignoredDirs" : '\*"/\\\[\]:;\|=,<>',   # / and \ are disallowed
+    }
+    patterns = {
+        "paths"       : re.compile('^(?:([^' + forbidden_characters["paths"]       + ']+):)*[^' + forbidden_characters["paths"]       + ']+$'),
+        "extensions"  : re.compile('^(?:([^' + forbidden_characters["extensions"]  + ']+):)*[^' + forbidden_characters["extensions"]  + ']+$'),
+        "ignoredDirs" : re.compile('^(?:([^' + forbidden_characters["ignoredDirs"] + ']+):)*[^' + forbidden_characters["ignoredDirs"] + ']+$'),
+    }
+
+
+    def __init__(self, conditions = None):
+        self.initialized = False
+        self.conditions = {}
+        self.pattern = None
+        if conditions is not None:
+            self.set_conditions(conditions)
+
+
+    def set_conditions(self, conditions):
+        """Validate and then set the conditions of this Filter"""
+        present_conditions = Set(conditions.keys())
+
+        # Ensure all required conditions are set.
+        if len(self.__class__.required_conditions.difference(present_conditions)):
+            raise MissingConditionError
+
+        # Ensure only valid conditions are set.
+        if len(present_conditions.difference(self.__class__.valid_conditions)):
+            raise InvalidConditionError
+
+        # Validate conditions. This may trigger exceptions, which should be
+        # handled by the caller.
+        self.__validate_conditions(conditions)
+        
+        # The conditions passed all validation tests: store it.
+        self.conditions = conditions
+
+        # Precompile the pattern condition, if there is one.
+        if (self.conditions.has_key("pattern")):
+            self.pattern = re.compile(self.conditions["pattern"])
+
+        self.initialized = True
+
+        return True
+
+
+    def __validate_conditions(self, conditions):
+        """Validate a given set of conditions"""
+
+        # The paths condition must contain paths separated by colons.
+        if not self.__class__.patterns["paths"].match(conditions["paths"]):
+            raise InvalidPathsConditionError
+
+        # The extensions condition must contain extensions separated by colons.
+        if not self.__class__.patterns["extensions"].match(conditions["extensions"]):
+            raise InvalidExtensionsConditionError
+
+        # The ignoredDirs condition must contain dirnames separated by colons.
+        if conditions.has_key("ignoredDirs"):
+            if not self.__class__.patterns["ignoredDirs"].match(conditions["ignoredDirs"]):
+                raise InvalidIgnoredDirsConditionError
+
+        # If a pattern condition is set, ensure that it's got a valid regular
+        # expression.
+        if conditions.has_key("pattern"):
+            try:
+                re.compile(conditions["pattern"])
+            except re.error:
+                raise InvalidPatternConditionError
+
+        # If a size condition is set, ensure that it's got both a size
+        # condition type and a treshold. And both of them must be valid.
+        if conditions.has_key("size"):
+            size = conditions["size"]
+            if len(self.__class__.required_sizeconditions.difference(size.keys())):
+                raise InvalidSizeConditionError, "The 'size' condition misses either of 'conditionType' and 'treshold'"
+            if size["conditionType"] != "minimum" and size["conditionType"] != "maximum":
+                raise InvalidSizeConditionError, "The 'size' condition has an invalid 'conditionType', valid values are 'maximum' and 'minimum'"
+            if type(size["treshold"]) != types.IntType:
+                raise InvalidSizeConditionError, "The 'size' condition has an invalid 'treshold', only integer values are valid'"
+
+
+    def matches(self, filepath, statfunc = os.stat):
+        """Check if the given filepath matches the conditions of this Filter
+
+        This function performs the different checks in an order that is
+        optimized for speed: the conditions that are most likely to reduce
+        the chance of a match are performed first.
+
+        """
+
+        if not self.initialized:
+            return False
+
+        match = True
+        (root, ext) = os.path.splitext(filepath)
+
+        # Step 1: apply the paths condition.
+        append_slash = lambda path: path + "/"
+        paths = map(append_slash, self.conditions["paths"].split(":"))
+        path_found = False
+        for path in paths:
+            if root.find(path) > -1:
+                path_found = True
+                break
+        if not path_found:
+            match = False
+        
+        # Step 2: apply the extensions condition.
+        if match:
+            ext = ext.lstrip(".")
+            if not ext in self.conditions["extensions"].split(":"):
+                match = False
+
+        # Step 3: apply the ignoredDirs condition.
+        if match and self.conditions.has_key("ignoredDirs"):
+            ignored_dirs = Set(self.conditions["ignoredDirs"].split(":"))
+            dirs = Set(root.split(os.sep))
+            if len(ignored_dirs.intersection(dirs)):
+                match = False
+
+        # Step 4: apply the pattern condition.
+        if match and self.conditions.has_key("pattern"):
+            if not self.pattern.match(filepath):
+                match = False
+
+        # Step 5: apply the size condition.
+        if match and self.conditions.has_key("size"):
+            size = statfunc(filepath)[stat.ST_SIZE]
+            condition_type = self.conditions["size"]["conditionType"]
+            treshold       = self.conditions["size"]["treshold"]
+            if condition_type == "minimum" and not treshold < size:
+                match = False
+            elif condition_type == "maximum" and not treshold > size:
+                match = False
+
+        return match
---
+++ code/daemon/filtertest.py
@@ -0,0 +1,184 @@
+"""Unit test for filter.py"""
+
+
+__author__ = "Wim Leers (work@wimleers.com)"
+__version__ = "$Rev$"
+__date__ = "$Date$"
+__license__ = "GPL"
+
+
+from filter import *
+import unittest
+
+
+class TestConditions(unittest.TestCase):
+    def setUp(self):
+        self.filter = Filter()
+
+    def testNoConditions(self):
+        """Filter should fail when no settings are provided"""
+        self.assertRaises(MissingConditionError, self.filter.set_conditions, {})
+
+    def testMissingConditions(self):
+        """Filter should fail when some settings are missing"""
+        self.assertRaises(MissingConditionError, self.filter.set_conditions, {"extensions" : "gif:png"})
+
+    def testInvalidConditions(self):
+        """Filter should fail when there is an invalid setting"""
+        self.assertRaises(InvalidConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz", "invalid" : "invalid"})
+
+    def testValidConditions(self):
+        """setting filter conditions should return true when all required and no invalid conditions are specified"""
+        # The minimal valid filter conditions.
+        self.assertTrue(self.filter.set_conditions({"extensions" : "gif:png", "paths" : "foo/bar:baz"}))
+        # The maximal valid filter conditions.
+        self.assertTrue(self.filter.set_conditions({"extensions" : "gif:png", "paths" : "foo/bar:baz", "ignoredDirs" : ".svn:CVS", "pattern" : "foo/bar", "size" : { "conditionType" : "minimum", "treshold" : 1000000}}))
+
+    def testInvalidPathsCondition(self):
+        """Filter should fail when setting an invalid paths filter condition"""
+        self.assertRaises(InvalidPathsConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo:baz>"})
+        # Special: / is allowed
+        self.assertRaises(InvalidPathsConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz>"})
+        # Special: \ is allowed
+        self.assertRaises(InvalidPathsConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo\\bar:baz>"})
+
+    def testInvalidExtensionsCondition(self):
+        """Filter should fail when setting an invalid extensions filter condition"""
+        self.assertRaises(InvalidExtensionsConditionError, self.filter.set_conditions, {"extensions" : "<gif:png", "paths" : "foo/bar:baz"})
+        # Special: . is disallowed
+        self.assertRaises(InvalidExtensionsConditionError, self.filter.set_conditions, {"extensions" : ".gif:png", "paths" : "foo/bar:baz"})
+        # Special: / is disallowed
+        self.assertRaises(InvalidExtensionsConditionError, self.filter.set_conditions, {"extensions" : "/gif:png", "paths" : "foo/bar:baz"})
+        # Special: \ is disallowed
+        self.assertRaises(InvalidExtensionsConditionError, self.filter.set_conditions, {"extensions" : "\\gif:png", "paths" : "foo/bar:baz"})
+        
+    def testInvalidIgnoredDirsCondition(self):
+        """Filter should fail when setting an invalid ignoredDirs filter condition"""
+        self.assertRaises(InvalidIgnoredDirsConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz", "ignoredDirs" : ".svn:CVS/"})
+        # Special: / is disallowed
+        self.assertRaises(InvalidIgnoredDirsConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz", "ignoredDirs" : ".svn:CVS/"})
+        # Special: \ is disallowed
+        self.assertRaises(InvalidIgnoredDirsConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz", "ignoredDirs" : ".svn:\\CVS"})
+
+    def testInvalidPatternCondition(self):
+        """Filter should fail when setting an invalid pattern filter condition"""
+        self.assertRaises(InvalidPatternConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz", "pattern" : "foo(bar"})
+
+    def testInvalidSizeCondition(self):
+        """Filter should fail when setting an invalid size filter condition"""
+        # Missing conditionType
+        self.assertRaises(InvalidSizeConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz", "size" : {"treshold" : 1000000}})
+        # Missing treshold
+        self.assertRaises(InvalidSizeConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz", "size" : {"conditionType" : "minimum"}})
+        # Invalid conditionType
+        self.assertRaises(InvalidSizeConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz", "size" : {"treshold" : 1000000, "conditionType" : "this is in an invalid condition type"}})
+        # Invalid treshold
+        self.assertRaises(InvalidSizeConditionError, self.filter.set_conditions, {"extensions" : "gif:png", "paths" : "foo/bar:baz", "size" : {"conditionType" : "minimum", "treshold" : "this is not numeric and therefor invalid"}})
+
+    def testValidSizeCondition(self):
+        """'maximum' and 'minimum' are the allowed conditionTypes for the size condition"""
+        self.assertTrue(self.filter.set_conditions({"extensions" : "gif:png", "paths" : "foo/bar:baz", "size" : { "conditionType" : "minimum", "treshold" : 1000000}}))
+        self.assertTrue(self.filter.set_conditions({"extensions" : "gif:png", "paths" : "foo/bar:baz", "size" : { "conditionType" : "maximum", "treshold" : 1000000}}))
+
+
+class TestMatching(unittest.TestCase):
+    def testWithoutConditions(self):
+        """Ensure matching works properly even when no conditions are set"""
+        filter = Filter()
+        self.assertFalse(filter.matches('whatever'))
+
+    def testSimpleMatches(self):
+        """Ensure paths/extensions matching works properly"""
+        conditions = {
+            "extensions" : "gif:png",
+            "paths" : "foo/bar:baz"
+        }
+        filter = Filter(conditions)
+        # Invalid extensions, valid paths
+        self.assertFalse(filter.matches('/a/foo/bar/b.mov'))
+        self.assertFalse(filter.matches('/a/baz/c/d/e/f.txt'))
+        # Invalid paths, valid extensions
+        self.assertFalse(filter.matches('/a/b.png'))
+        self.assertFalse(filter.matches('/a/b/c/d/e/f.gif'))
+        # Both invalid extensions and paths
+        self.assertFalse(filter.matches('/a/b.rar'))
+        self.assertFalse(filter.matches('/a/b/c/d/e/f.avi'))
+        # Both valid extensions and paths
+        self.assertTrue(filter.matches('/a/b/c/d/e/foo/bar/g.gif'))
+        self.assertTrue(filter.matches('/a/baz/c.png'))
+        # Tricky one: the path seems to match, but is part of the filename and
+        # therefor it doesn't match!
+        self.assertFalse(filter.matches('foo/bar.gif'))
+        self.assertFalse(filter.matches('baz.png'))
+
+    def testIgnoredDirsMatches(self):
+        """Ensure ignoredDirs matching works properly"""
+        conditions = {
+            "extensions" : "gif:png",
+            "paths" : "foo/bar:baz",
+            "ignoredDirs" : ".svn:CVS",
+        }
+        filter = Filter(conditions)
+        # Contains ignored dirs
+        self.assertFalse(filter.matches('/a/foo/bar/.svn/b.gif'))
+        self.assertFalse(filter.matches('/a/baz/CVS/d/e/f.png'))
+        # Doesn't contain ignored dirs
+        self.assertTrue(filter.matches('/a/b/c/d/e/foo/bar/g.gif'))
+        self.assertTrue(filter.matches('/a/baz/c.png'))
+    
+    def testPatternMatches(self):
+        """Ensure pattern matching works properly"""
+        conditions = {
+            "extensions" : "gif:png",
+            "paths" : "foo/bar:baz",
+            "ignoredDirs" : ".svn:CVS",
+        }
+        filter = Filter(conditions)
+        # Does not match pattern
+        self.assertFalse(filter.matches('/a/foo/bar/.svn/b.gif'))
+        self.assertFalse(filter.matches('/a/baz/CVS/d/e/f.png'))
+        # Matches pattern
+        self.assertTrue(filter.matches('/a/b/c/d/e/foo/bar/this_one_has_underscores.gif'))
+        self.assertTrue(filter.matches('/a/and_this_one_too/baz/c.png'))
+    
+    def testSizeMatches(self):
+        """Ensure size validation works properly"""
+
+        # The matches function only looks at ST_SIZE, which is in the 7th
+        # position in the tuple. This lambda function simplifies the rest of
+        # this test case.
+        fakestatfunc = lambda filesize: (1, 2, 3, 4, 5, 6, filesize)
+        # We always use the same filepath.
+        filepath = '/a/baz/c.png'
+
+        # Minimum size
+        conditions = {
+            "extensions" : "gif:png",
+            "paths" : "foo/bar:baz",
+            "ignoredDirs" : ".svn:CVS",
+            "size" : {
+                "conditionType" : "minimum",
+                "treshold" : 500
+            }
+        }
+        filter = Filter(conditions)
+        # Meets minimum size
+        statfunc = lambda filepath: fakestatfunc(501L)
+        self.assertTrue(filter.matches(filepath, statfunc))
+        # Does not meet minimum size
+        statfunc = lambda filepath: fakestatfunc(499L)
+        self.assertFalse(filter.matches(filepath, statfunc))
+
+        # Maximium size
+        conditions["size"]["conditionType"] = "maximum"
+        filter.set_conditions(conditions)
+        # Meets maximum size
+        statfunc = lambda filepath: fakestatfunc(499L)
+        self.assertTrue(filter.matches(filepath, statfunc))
+        # Does not meet maximum size
+        statfunc = lambda filepath: fakestatfunc(500L)
+        self.assertFalse(filter.matches(filepath, statfunc))
+
+
+if __name__ == "__main__":
+    unittest.main()
