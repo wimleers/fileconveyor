f2f6d0bb5a5db2c0a26bec0820424b1e0300516c
Wim Leers <work@wimleers.com>
2009-04-30 19:24:09 +0000

    Added the "none" transporter, which makes it possible to use automatically offloading CDNs or a static file server that's running on the same server (same OS instance).

---
+++ code/daemon/transporters/transporter_none.py
@@ -0,0 +1,48 @@
+from transporter import *
+from storages.SymlinkOrCopyStorage import *
+
+
+class TransporterNone(Transporter):
+
+
+    valid_settings    = ImmutableSet(["location", "url", "symlinkWithin"])
+    required_settings = ImmutableSet(["location", "url", "symlinkWithin"])
+
+
+    def __init__(self, settings, callback):
+        Transporter.__init__(self, settings, callback)
+
+        # Validate settings.
+        configured_settings = Set(self.settings.keys())
+        Transporter.validate_settings(self, self.__class__.valid_settings, self.__class__.required_settings, configured_settings)
+
+        # Map the settings to the format expected by SymlinkStorage.
+        self.storage = SymlinkOrCopyStorage(self.settings["location"],
+                                            self.settings["url"],
+                                            self.settings["symlinkWithin"]
+                                            )
+
+
+if __name__ == "__main__":
+    import time
+    import subprocess
+    import tempfile
+    import os.path
+
+    def callbackfunc(filepath, url):
+        print "CALLBACK FIRED: filepath=%s, url=%s" % (filepath, url)
+
+    settings = {
+        "location"     : "/htdocs/static.example.com/",
+        "url"          : "http://static.example.com/",
+        "symlinkWithin": os.path.abspath('')
+    }
+    none = TransporterNone(settings, callbackfunc)
+    none.start()
+    none.sync_file("transporter.py")
+    none.sync_file("drupal-5-6.png")
+    none.sync_file("subdir/bmi-chart.png")
+    subprocess.call("echo yarhar > $TMPDIR/foobar.txt && chmod 777 $TMPDIR/foobar.txt", shell=True, stdout=subprocess.PIPE)
+    none.sync_file(os.path.join(tempfile.gettempdir(), "foobar.txt"))
+    time.sleep(5)
+    none.stop()
