9515c2784423edda209ac826cca29c0b77135ce9
Wim Leers <work@wimleers.com>
2009-05-05 14:41:15 +0000

    New: DaemonThreadRunner class, which allows you to use signals to kill the daemon, just like Apache :)

--- code/daemon/arbitrator.py
+++ code/daemon/arbitrator.py
@@ -551,29 +551,70 @@ class Arbitrator(threading.Thread):
         self.lock.release()
 
 
+class DaemonThreadRunner(object):
+    """runs a thread as a daemon and provides a PID file through which you can
+    kill the daemon (kill -TERM `cat pidfile`)"""
+
+    pidfile_check_interval = 60
+    pidfile_permissions    = 0600
+
+    def __init__(self, thread, pidfilename):
+        if not pidfilename.endswith(".pid"):
+            pidfilename = pidfilename + ".pid"
+
+        self.thread             = thread
+        self.running            = False
+        self.pidfile            = os.path.join(os.getcwd(), pidfilename)
+        self.last_pidfile_check = 0
+
+        # Configure signal handler.
+        signal.signal(signal.SIGINT,  self.handle_interrupt)
+        signal.signal(signal.SIGTSTP, self.handle_interrupt)
+        signal.signal(signal.SIGTERM, self.handle_interrupt)
+
+
+    def start(self):
+        self.write_pid_file()
+
+        # Start the daemon thread.
+        self.running = True
+        self.thread.setDaemon(True)
+        self.thread.start()
+
+        # While running, keep the PID file updated and sleep.
+        while self.running:
+            self.update_pid_file()
+            time.sleep(1)
+
+        # Remove the PID file.
+        if os.path.isfile(self.pidfile):
+            os.remove(self.pidfile)
+
+
+    def handle_interrupt(self, signalNumber, frame):
+        self.thread.stop()
+        self.thread.join()
+        self.running = False
+
+
+    def write_pid_file(self):
+        pid = os.getpid()
+        open(self.pidfile, 'w+').write(str(pid))
+        os.chmod(self.pidfile, self.pidfile_permissions)
+
+
+    def update_pid_file(self):
+        # Recreate the file when it is deleted.
+        if not os.path.isfile(self.pidfile):
+            self.write_pid_file()
+
+        # Update the file every interval.
+        if self.last_pidfile_check + self.pidfile_check_interval < time.time():
+            self.write_pid_file()
+            self.last_pidfile_check = time.time()
+
+
 if __name__ == '__main__':
     arbitrator = Arbitrator("config.sample.xml")
-    arbitrator.start()
-    if arbitrator.isAlive():
-        time.sleep(30)
-        arbitrator.stop()
-        arbitrator.join()
-    
-
-    # def handleKeyboardInterrupt(signalNumber, frame):
-    #     print "stopping"
-    #     arbitrator.stop()
-    #     print "stopped!"
-    # 
-    # try :
-    #     # Register a signal handler for ctrl-C, control-z
-    #     signal.signal(signal.SIGINT, handleKeyboardInterrupt)
-    #     signal.signal(signal.SIGTSTP, handleKeyboardInterrupt)
-    # 
-    #     # Signal doesn't work.
-    #     arbitrator.start()
-    # 
-    #     # Signal works (because no separate thread is started).
-    #     #arbitrator.run()
-    # finally:
-    #     pass
+    t = DaemonThreadRunner(arbitrator, "daemon")
+    t.start()
