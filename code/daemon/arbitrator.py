23d2ac0cb7fd8b4d8aecff79a04fda287e7464fe
Wim Leers <work@wimleers.com>
2009-05-05 15:08:12 +0000

    Moved the DaemonThreadRunner class into its own module.

--- code/daemon/arbitrator.py
+++ code/daemon/arbitrator.py
@@ -5,7 +5,6 @@ import os
 import stat
 import threading
 import time
-import signal
 import sys
 from UserList import UserList
 
@@ -20,6 +19,7 @@ from fsmonitor import *
 from filter import *
 from processors.processor import *
 from transporters.transporter import *
+from daemon_thread_runner import *
 
 
 LOG_FILE = './daemon.log'
@@ -556,69 +556,6 @@ class Arbitrator(threading.Thread):
         self.lock.acquire()
         self.die = True
         self.lock.release()
-
-
-class DaemonThreadRunner(object):
-    """runs a thread as a daemon and provides a PID file through which you can
-    kill the daemon (kill -TERM `cat pidfile`)"""
-
-    pidfile_check_interval = 60
-    pidfile_permissions    = 0600
-
-    def __init__(self, thread, pidfilename):
-        if not pidfilename.endswith(".pid"):
-            pidfilename = pidfilename + ".pid"
-
-        self.thread             = thread
-        self.running            = False
-        self.pidfile            = os.path.join(os.getcwd(), pidfilename)
-        self.last_pidfile_check = 0
-
-        # Configure signal handler.
-        signal.signal(signal.SIGINT,  self.handle_interrupt)
-        signal.signal(signal.SIGTSTP, self.handle_interrupt)
-        signal.signal(signal.SIGTERM, self.handle_interrupt)
-
-
-    def start(self):
-        self.write_pid_file()
-
-        # Start the daemon thread.
-        self.running = True
-        self.thread.setDaemon(True)
-        self.thread.start()
-
-        # While running, keep the PID file updated and sleep.
-        while self.running:
-            self.update_pid_file()
-            time.sleep(1)
-
-        # Remove the PID file.
-        if os.path.isfile(self.pidfile):
-            os.remove(self.pidfile)
-
-
-    def handle_interrupt(self, signalNumber, frame):
-        self.thread.stop()
-        self.thread.join()
-        self.running = False
-
-
-    def write_pid_file(self):
-        pid = os.getpid()
-        open(self.pidfile, 'w+').write(str(pid))
-        os.chmod(self.pidfile, self.pidfile_permissions)
-
-
-    def update_pid_file(self):
-        # Recreate the file when it is deleted.
-        if not os.path.isfile(self.pidfile):
-            self.write_pid_file()
-
-        # Update the file every interval.
-        if self.last_pidfile_check + self.pidfile_check_interval < time.time():
-            self.write_pid_file()
-            self.last_pidfile_check = time.time()
 
 
 if __name__ == '__main__':
---
+++ code/daemon/daemon_thread_runner.py
@@ -0,0 +1,76 @@
+"""transporter.py Transporter class for daemon"""
+
+
+__author__ = "Wim Leers (work@wimleers.com)"
+__version__ = "$Rev$"
+__date__ = "$Date$"
+__license__ = "GPL"
+
+
+import os
+import os.path
+import signal
+import time
+
+
+class DaemonThreadRunner(object):
+    """runs a thread as a daemon and provides a PID file through which you can
+    kill the daemon (kill -TERM `cat pidfile`)"""
+
+    pidfile_check_interval = 60
+    pidfile_permissions    = 0600
+
+    def __init__(self, thread, pidfilename):
+        if not pidfilename.endswith(".pid"):
+            pidfilename = pidfilename + ".pid"
+
+        self.thread             = thread
+        self.running            = False
+        self.pidfile            = os.path.join(os.getcwd(), pidfilename)
+        self.last_pidfile_check = 0
+
+        # Configure signal handler.
+        signal.signal(signal.SIGINT,  self.handle_interrupt)
+        signal.signal(signal.SIGTSTP, self.handle_interrupt)
+        signal.signal(signal.SIGTERM, self.handle_interrupt)
+
+
+    def start(self):
+        self.write_pid_file()
+
+        # Start the daemon thread.
+        self.running = True
+        self.thread.setDaemon(True)
+        self.thread.start()
+
+        # While running, keep the PID file updated and sleep.
+        while self.running:
+            self.update_pid_file()
+            time.sleep(1)
+
+        # Remove the PID file.
+        if os.path.isfile(self.pidfile):
+            os.remove(self.pidfile)
+
+
+    def handle_interrupt(self, signalNumber, frame):
+        self.thread.stop()
+        self.thread.join()
+        self.running = False
+
+
+    def write_pid_file(self):
+        pid = os.getpid()
+        open(self.pidfile, 'w+').write(str(pid))
+        os.chmod(self.pidfile, self.pidfile_permissions)
+
+
+    def update_pid_file(self):
+        # Recreate the file when it is deleted.
+        if not os.path.isfile(self.pidfile):
+            self.write_pid_file()
+
+        # Update the file every interval.
+        if self.last_pidfile_check + self.pidfile_check_interval < time.time():
+            self.write_pid_file()
+            self.last_pidfile_check = time.time()
