029e5ea75be8636a9fd65a0fc23399358010bb04
Wim Leers <work@wimleers.com>
2009-05-19 15:32:58 +0000

    More strict validation of sources.

--- code/daemon/config.py
+++ code/daemon/config.py
@@ -68,13 +68,22 @@ class Config(object):
             if not self.source_name_regex.match(name):
                 self.logger.error("The name '%s' for a source is invalid. Only use alphanumeric characters, the dash and the underscore." % (name))
                 self.errors += 1
-            if not scan_path is None and not os.path.exists(scan_path):
+            if scan_path is None:
+                self.logger.error("The %s scan path is not configured." % (name))
+                self.errors += 1                
+            elif not os.path.exists(scan_path):
                 self.logger.error("The %s scan path ('%s') does not exist." % (name, scan_path))
                 self.errors += 1
-            if not document_root is None and not os.path.exists(document_root):
+            if document_root is None:
+                self.logger.error("The %s document root is not configured." % (name))
+                self.errors += 1                
+            elif not os.path.exists(document_root):
                 self.logger.error("The %s document root ('%s') does not exist." % (name, document_root))
                 self.errors += 1
-            if not base_path is None and (base_path[0] != "/" or base_path[-1] != "/"):
+            if base_path is None:
+                self.logger.error("The %s base path is not configured." % (name))
+                self.errors += 1                
+            elif base_path[0] != "/" or base_path[-1] != "/":
                 self.logger.error("The %s base path ('%s') is invalid. It should have both leading and trailing slashes." % (name, base_path))
                 self.errors += 1
             if not document_root is None and not base_path is None:
