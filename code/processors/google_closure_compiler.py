c1a65072863fb4e21a7cd2259090996496450a42
Wim Leers <work@wimleers.com>
2010-04-20 11:08:01 +0200

    Fix a stupid typo in the Google Closure Compiler processor that prevented it from working properly.

--- code/processors/google_closure_compiler.py
+++ code/processors/google_closure_compiler.py
@@ -21,7 +21,7 @@ class GoogleClosureCompiler(Processor):
 
         # Run Google Closure Compiler on the file.
         compiler_path = os.path.join(self.processors_path, "compiler.jar")
-        (stdout, stderr) = self.run_command("java -jar %s --js %s --js_output_file %s" % (compiler_path, self.input_file, self.input_file))
+        (stdout, stderr) = self.run_command("java -jar %s --js %s --js_output_file %s" % (compiler_path, self.input_file, self.output_file))
 
         # Raise an exception if an error occurred.
         if not stderr == "":
