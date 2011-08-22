905ecd73446f2d6f0cb8761b93b84412030adf4a
Benoit Bryon <benoit.bryon@makina-corpus.com>
2011-08-22 16:19:59 +0200

    Packaged fileconveyor as an egg, in order to be compatible with Python tools like easy_install, pip or buildout. Added setup.py. Imports can now be done with fileconveyor namespace. In configuration file, transporters and processors names are full python module names (i.e. like fileconveyor.transporters.transporter_symlink_or_copy).

---
+++ setup.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+import os.path
+from setuptools import setup, find_packages
+
+
+def read_relative_file(filename):
+    """Returns contents of the given file.
+    Filename argument must be relative to this module.
+    """
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
+
+
+setup(
+    name='fileconveyor',
+    version='0.1-dev',
+    url='http://fileconveyor.org',
+    download_url='https://github.com/wimleers/fileconveyor',
+    author='Wim Leers',
+    license='Unlicense',
+    description="Daemon to detect, process and sync files to CDNs.",
+    long_description=read_relative_file('README.txt'),
+    platforms='Any',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    include_package_data = True,
+    install_requires=[
+        'setuptools',
+        'cssutils',
+    ],
+)
