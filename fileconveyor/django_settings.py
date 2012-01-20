6d95a663b6c313217d991bb78a6eb36ef6a0a8a9
Wim Leers <work@wimleers.com>
2012-01-20 17:02:26 +0100

    Dummy settings for django-storages.

---
+++ fileconveyor/django_settings.py
@@ -0,0 +1,12 @@
+# Dummy settings for `django-storages`.
+MEDIA_URL=''
+MEDIA_ROOT=''
+# `backends/ftp.py`
+FTP_STORAGE_LOCATION=''
+# `backends/mosso.py`
+CLOUDFILES_USERNAME=''
+CLOUDFILES_API_KEY=''
+CLOUDFILES_CONTAINER=''
+CONNECTION_KWARGS=[]
+# `backends/sftp.py`
+SFTP_STORAGE_HOST=''
